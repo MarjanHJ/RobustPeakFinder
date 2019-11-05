f613b103e935ad8c7a12f010164bae3cf77f2934
ARSadri <51805937+ARSadri@users.noreply.github.com>
2019-11-05 03:25:26 +0100

    Update RobustPeakFinder_Python_Wrapper.py

--- RobustPeakFinder_Python_Wrapper.py
+++ RobustPeakFinder_Python_Wrapper.py
@@ -11,14 +11,16 @@ LAMBDA : The ratio of a Guassian Profile over its standard deviation that is ass
 		default: 4 Sigma (Sigma being its STD)
 SNR_ACCEPT: Traditionally, SNR is one of the factors to reject bad peakListCheetah
 		default: 8.0
-PEAK_MAX_PIX: Number of pixels in a peak.
+PEAK_MAX_PIX: Maximum number of pixels in a peak.
 		default: 50
+PEAK_MIN_PIX: Minimum number of pixels in a peak.
+		default: 1
 
 Output:
 peakListCheetah is a numpy 2D-array in the style of Cheetah's output.
 Rows are peaks and coloums are:
 -------------------------------------------------------------------------
-Mass_Center_X, Mass_Center_Y, Mass_Total, Number of pixels in a peak
+Mass_Center_X, Mass_Center_Y, Mass_Total, Number of pixels in a peak, maximum intensity, SNR
 -------------------------------------------------------------------------
 
 You can get the number of peaks by YOUROUTPUTNAME.shape[0]
@@ -32,14 +34,15 @@ peakFinderPythonLib.peakFinder.argtypes = [
 				ctypes.c_double, ctypes.c_double,
 				numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
 				numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                ctypes.c_int, ctypes.c_int, ctypes.c_int,
+                ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int,
                 numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
 
 
 def robustPeakFinderPyFunc(inData, inMask = None,
 				LAMBDA = 4.0,
 				SNR_ACCEPT = 8.0,
-				PEAK_MAX_PIX = 50):
+				PEAK_MAX_PIX = 50,
+			        PEAK_MIN_PIX = 1):
     inData = numpy.double(inData)
     if(inMask is None):
         inMask = 1 + 0*inData
@@ -49,5 +52,5 @@ def robustPeakFinderPyFunc(inData, inMask = None,
     szx, szy = inData.shape
     peak_cnt = peakFinderPythonLib.peakFinder(LAMBDA, SNR_ACCEPT,
 						inData, inMask, szy, szx,
-						PEAK_MAX_PIX, peakListCheetah)
+						PEAK_MAX_PIX, PEAK_MIN_PIX, peakListCheetah)
     return peakListCheetah[:peak_cnt]
