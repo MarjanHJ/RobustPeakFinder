2d5b2af9ee81379cc1b1fbe42e6c3aaa08d45017
ARSadri <51805937+ARSadri@users.noreply.github.com>
2019-11-20 13:51:31 +0100

    Update RobustPeakFinder_Python_Wrapper.py

--- RobustPeakFinder_Python_Wrapper.py
+++ RobustPeakFinder_Python_Wrapper.py
@@ -1,7 +1,7 @@
 '''
 The Python Wrapper for the Robust Peak peakFinder
 Authors: Marjan Hadian Jazi
-		 Alireza Sadri
+	 Alireza Sadri
 
 Inputs:
 inData : This is the 2D input image as a numpy 2d-array.
@@ -11,16 +11,16 @@ LAMBDA : The ratio of a Guassian Profile over its standard deviation that is ass
 		default: 4 Sigma (Sigma being its STD)
 SNR_ACCEPT: Traditionally, SNR is one of the factors to reject bad peakListCheetah
 		default: 8.0
-PEAK_MAX_PIX: Maximum number of pixels in a peak.
+PEAK_MAX_PIX: maximum number of pixels in a peak.
 		default: 50
 PEAK_MIN_PIX: Minimum number of pixels in a peak.
-		default: 1
+		default: 50		
 
 Output:
 peakListCheetah is a numpy 2D-array in the style of Cheetah's output.
 Rows are peaks and coloums are:
 -------------------------------------------------------------------------
-Mass_Center_X, Mass_Center_Y, Mass_Total, Number of pixels in a peak, maximum intensity, SNR
+Mass_Center_X, Mass_Center_Y, Mass_Total, Number of pixels in a peak, Maximum value, SNR
 -------------------------------------------------------------------------
 
 You can get the number of peaks by YOUROUTPUTNAME.shape[0]
@@ -28,7 +28,7 @@ You can get the number of peaks by YOUROUTPUTNAME.shape[0]
 
 import numpy
 import ctypes
-peakFinderPythonLib = ctypes.cdll.LoadLibrary("./RobustPeakFinder.so")
+peakFinderPythonLib = ctypes.cdll.LoadLibrary("/home/sadriali/Projects/RPFOrig/Debug/libRPFOrig.so")
 peakFinderPythonLib.peakFinder.restype = ctypes.c_int
 peakFinderPythonLib.peakFinder.argtypes = [
 				ctypes.c_double, ctypes.c_double,
@@ -42,7 +42,7 @@ def robustPeakFinderPyFunc(inData, inMask = None,
 				LAMBDA = 4.0,
 				SNR_ACCEPT = 8.0,
 				PEAK_MAX_PIX = 50,
-			        PEAK_MIN_PIX = 1):
+				PEAK_MIN_PIX = 1):
     inData = numpy.double(inData)
     if(inMask is None):
         inMask = 1 + 0*inData
