eaaef6135b477fd486bd7da6093368db5ca8e316
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-04-29 04:20:24 +0200

    Update RobustPeakFinder_Python_Wrapper.py
    numpy.flatten runs by rows by default, make it a row array

--- RobustPeakFinder_Python_Wrapper.py
+++ RobustPeakFinder_Python_Wrapper.py
@@ -1,95 +1,108 @@
-'''
+"""
 The Python Wrapper for the Robust Peak peakFinder
 Authors: Marjan Hadian Jazi
-     Alireza Sadri
+         Alireza Sadri
 
 Inputs:
 inData : This is the 2D input image as a numpy 2d-array.
 inMask : This is the bad pixel mask.
         default: 1 for all pixels, with the size of inData
-maxBiasMap : maximum value of the model for the background
-        default: 32000 with the size of inData
-thresholdMap : a threshold that will be used added to the estimated model for the background 
+minPeakValMap : a threshold for peak maximum value
         default: 0 with the size of inData
-AbProbMap : an abnormality probability that is multplied to the SNR and also used to preform weighted regression for background
+SNRFactor : will be multiplied to SNR, useful if badpixel mask generates probabilities
         default: 1 with the size of inData
-StdsMap : standard deviation of variations of pixels in the background used in calculation of SNR
-        default: 0 with the size of inData
-LAMBDA : The ratio of a Guassian Profile over its standard deviation that is assumed as inlier
-        default: 4 Sigma (Sigma being its STD)
-SNR_ACCEPT: Traditionally, SNR is one of the factors to reject bad peakListCheetah
-        default: 8.0
-PEAK_MAX_PIX: maximum number of pixels in a peak.
-        default: 50
-PEAK_MIN_PIX: Minimum number of pixels in a peak.
-        default: 1        
+maxBackMeanMap : maximum value of the model for the background, 
+                   useful for stopping background to go to nonlinear regions
+        default: 32000 with the size of inData
+peakList : the output array of size MAXIMUM_NUMBER_OF_PEAKS x 6
+        default: all zeros
 MAXIMUM_NUMBER_OF_PEAKS: self explanatory
         default = 1024
+bckSNR: Background SNR
+        default: 6.0
+pixPAPR : pixels Peak to average power ratio
+        default: 2.0
+PEAK_MIN_PIX: Minimum number of pixels in a peak.
+        default: 1        
+PEAK_MAX_PIX: maximum number of pixels in a peak.
+        default: 25
         
 Output:
-peakListCheetah is a numpy 2D-array in the style of Cheetah's output.
-Rows are peaks and coloums are:
--------------------------------------------------------------------------
+peakListCheetah is a numpy 2D-array in the style of Cheetah's output for CXI files
+Each rows is for each peak and coloums are:
+-----------------------------------------------------------------------------------------
 Mass_Center_X, Mass_Center_Y, Mass_Total, Number of pixels in a peak, Maximum value, SNR
--------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
 You can get the number of peaks by YOUROUTPUTNAME.shape[0]
-'''
+"""
 
 import numpy
 import ctypes
-peakFinderPythonLib = ctypes.cdll.LoadLibrary("./RobustPeakFinder.so")
+peakFinderPythonLib = ctypes.cdll.LoadLibrary("./../RPFOrig/Debug/libRPFOrig.so")
+                
+'''
+int peakFinder(	float *inData, float *inMask,
+				float *SNRFactor, float *minPeakValMap, float *maxBackMeanMap, 
+				float *peakList, int MAXIMUM_NUMBER_OF_PEAKS,
+				float bckSNR, float pixPAPR,
+				int XPIX, int YPIX, int PTCHSZ,	
+				int PEAK_MIN_PIX, int PEAK_MAX_PIX)
+'''
 peakFinderPythonLib.peakFinder.restype = ctypes.c_int
 peakFinderPythonLib.peakFinder.argtypes = [
-                ctypes.c_double, ctypes.c_double,
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                numpy.ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
+                numpy.ctypeslib.ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
+                numpy.ctypeslib.ndpointer(ctypes.c_uint8, flags="C_CONTIGUOUS"),
+                numpy.ctypeslib.ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
+                numpy.ctypeslib.ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
+                numpy.ctypeslib.ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
+                numpy.ctypeslib.ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
+                ctypes.c_int,
+                ctypes.c_float, ctypes.c_float,
+                ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
 
 
 def robustPeakFinderPyFunc(inData, 
-                maxBiasMap = None, 
-                thresholdMap = None, 
-                AbProbMap = None, 
-                StdsMap = None, 
                 inMask = None,
-                LAMBDA = 4.0,
-                SNR_ACCEPT = 8.0,
+                minPeakValMap = None, 
+                maxBackMeanMap = None, 
+                SNRFactor = None, 
+                bckSNR = 6.0,
+                pixPAPR = 2.0,
+                PTCHSZ = 16,
                 PEAK_MAX_PIX = 25,
                 PEAK_MIN_PIX = 1,
                 MAXIMUM_NUMBER_OF_PEAKS = 1024):
-    inData = numpy.double(inData)
-    if(maxBiasMap is None):
-        maxBiasMap = 0*inData.copy() + 32000
-    else:
-        maxBiasMap = numpy.double(maxBiasMap)
-    if(thresholdMap is None):
-        thresholdMap = 0*inData.copy()
-    else:
-        thresholdMap = numpy.double(thresholdMap)
-    if(inMask is None):
-        inMask = 1 + 0*inData.copy()
-    else:
-        inMask = numpy.double(inMask)
-    if(AbProbMap is None):
-        AbProbMap = 1 + 0*inData.copy()
-    else:
-        AbProbMap = numpy.double(AbProbMap)
-    if(StdsMap is None):
-        StdsMap = 0*inData.copy()
-    else:
-        StdsMap = numpy.double(StdsMap)
     
-    #if you change the maximum numbre of peaks here, the C code must know about it.    
-    peakListCheetah = numpy.zeros([MAXIMUM_NUMBER_OF_PEAKS, 6])    
-    szx, szy = inData.shape
-    peak_cnt = peakFinderPythonLib.peakFinder(LAMBDA, SNR_ACCEPT,
-                        inData, AbProbMap, StdsMap, inMask, thresholdMap, maxBiasMap,
-                        szy, szx, PEAK_MAX_PIX, PEAK_MIN_PIX, MAXIMUM_NUMBER_OF_PEAKS,
-                        peakListCheetah)
+    inDataSize = inData.size
+    XPIX, YPIX = inData.shape
+    if(inMask is None):
+        inMask = numpy.ones(inDataSize, dtype='uint8')
+    else:
+        inMask = inMask.astype('uint8')
+    if(maxBackMeanMap is None):
+        maxBackMeanMap = numpy.ones(inDataSize, dtype='float32')*numpy.finfo('float32').max
+    else:
+        maxBackMeanMap = maxBackMeanMap.astype('float32')
+    if(minPeakValMap is None):
+        minPeakValMap = numpy.zeros(inDataSize, dtype='float32')
+    else:
+        minPeakValMap = minPeakValMap.astype('float32')
+    if(SNRFactor is None):
+        SNRFactor = numpy.ones(inDataSize, dtype='float32')
+    else:
+        SNRFactor = SNRFactor.astype('float32')
+    
+    inData = inData.astype('float32')
+    
+    peakListCheetah = numpy.zeros([MAXIMUM_NUMBER_OF_PEAKS, 6], dtype='float32')    
+    
+    peak_cnt = peakFinderPythonLib.peakFinder(inData.flatten('F'), inMask.flatten('F'), 
+                        SNRFactor.flatten('F'), minPeakValMap.flatten('F'), maxBackMeanMap.flatten('F'),
+                        peakListCheetah, MAXIMUM_NUMBER_OF_PEAKS,
+                        bckSNR, pixPAPR,
+                        XPIX, YPIX, PTCHSZ, PEAK_MIN_PIX, PEAK_MAX_PIX)
+
+    peakListCheetah[:, 0], peakListCheetah[:, 1] = peakListCheetah[:, 1], peakListCheetah[:, 0].copy()                    
+                        
     return peakListCheetah[:peak_cnt]
