a4c9d67790eafd61d6585c825d66b0be57c5d598
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-05-24 22:11:19 +0200

    Update RobustPeakFinder_Python_Test_Simple.py

--- RobustPeakFinder_Python_Test_Simple.py
+++ RobustPeakFinder_Python_Test_Simple.py
@@ -11,23 +11,15 @@ from os import getpid
 import numpy
 import scipy.stats
 
-numpy.set_printoptions(precision=2, suppress=True)
 
-def gkern(kernlen=21, nsig=3):
+def gkern(kernlen):
     lim = kernlen//2 + (kernlen % 2)/2
     x = numpy.linspace(-lim, lim, kernlen+1)
     kern1d = numpy.diff(scipy.stats.norm.cdf(x))
     kern2d = numpy.outer(kern1d, kern1d)
     return kern2d/(kern2d.flatten().max())
 
-if __name__ == '__main__':    
-    print('PID ->' + str(getpid()))
-    XSZ = 1020
-    YSZ = 980
-    WINSIZE = 21
-    inputPeaksNumber = 30
-    print("Generating a pattern with " + str(inputPeaksNumber) + " peaks...")
-
+def diffractionPatternMaker(XSZ, YSZ, WINSIZE, inputPeaksNumber, numOutliers):    
     inData = numpy.zeros((XSZ, YSZ), dtype='float32')
     
     inMask = numpy.ones(inData.shape, dtype = 'uint8')
@@ -38,29 +30,46 @@ if __name__ == '__main__':
     
     for ccnt in range(inData.shape[1]):
         for rcnt in range(inData.shape[0]):
-            inData[rcnt, ccnt] += 400*numpy.exp(-(((rcnt-512)**2+(ccnt-512)**2)**0.5 - 250)**2/(2*75**2))
+            inData[rcnt, ccnt] += 100 + 400*numpy.exp(-(((rcnt-512)**2+(ccnt-512)**2)**0.5 - 250)**2/(2*75**2))
             inData[rcnt, ccnt] += 3*numpy.sqrt(inData[rcnt, ccnt])*numpy.random.randn(1)    
     
-
-    randomLocations = XSZ/2 + numpy.floor(XSZ*0.8*(numpy.random.rand(2,inputPeaksNumber) - 0.5))
+    randomLocations = numpy.random.rand(2,inputPeaksNumber)
+    randomLocations[0,:] = XSZ/2 + numpy.floor(XSZ*0.8*(randomLocations[0,:] - 0.5))
+    randomLocations[1,:] = YSZ/2 + numpy.floor(YSZ*0.8*(randomLocations[1,:] - 0.5))
+    
     for cnt in numpy.arange(inputPeaksNumber):    
-        bellShapedCurve = 400*gkern(WINSIZE, nsig = 1)
+        bellShapedCurve = 600*gkern(WINSIZE)
         winXStart = (randomLocations[0, cnt] - (WINSIZE-1)/2).astype(numpy.int)
         winXEnd = (randomLocations[0, cnt] + (WINSIZE+1)/2).astype(numpy.int)
         winYStart = (randomLocations[1, cnt] - (WINSIZE-1)/2).astype(numpy.int)
         winYEnd = (randomLocations[1, cnt] + (WINSIZE+1)/2).astype(numpy.int)
         inData[ winXStart : winXEnd, winYStart : winYEnd ] += bellShapedCurve;
-        if (cnt >= 25):
-            inMask[ winXStart : winXEnd, winYStart : winYEnd ] = 0;
+        if (cnt >= inputPeaksNumber - numOutliers):
+            inMask[ winXStart : winXEnd, winYStart : winYEnd ] = 0;    
+    
+    return(inData, inMask, randomLocations)
+    
+numpy.set_printoptions(precision=2, suppress=True)
+   
+if __name__ == '__main__':    
+    print('PID ->' + str(getpid()))
+    XSZ = 1024
+    YSZ = 976
+    WINSIZE = 7
+    inputPeaksNumber = 25
+    numOutliers = 5
+    print("Generating a pattern with " + str(inputPeaksNumber) + " peaks...")
+    
+    inData, inMask, randomLocations = diffractionPatternMaker(XSZ, YSZ, WINSIZE, inputPeaksNumber, numOutliers)
 
     print("Pattern Ready! Calling the Robust Peak Finder...")
-    outdata = RobustPeakFinder_Python_Wrapper.robustPeakFinderPyFunc(inData = inData, inMask = inMask)
-    print("RPF: There is " + str(outdata.shape[0]) + " peaks in this image!")
+    outdata = RobustPeakFinder_Python_Wrapper.robustPeakFinderPyFunc(inData = inData, inMask = inMask, bckSNR=5.0)
+    print("RPF: There are " + str(outdata.shape[0]) + " peaks in this image!")
     print(outdata[:, :2].T)
     print(randomLocations)
-
+    
     plt.imshow((inData*inMask).T)
-    plt.scatter(randomLocations[0], randomLocations[1], s=40, facecolors='none', edgecolors='c')
-    plt.scatter(outdata[:, 0], outdata[:, 1], s=40, facecolors='none', edgecolors='r')
+    plt.plot(outdata[:, 0], outdata[:, 1],'o')
+    plt.plot(randomLocations[0,:], randomLocations[1,:],'x')
     plt.show()
     exit()
